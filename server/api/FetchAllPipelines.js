7e4c96f4277bc7f549dd2f69e50a441652c1c3cf
CIPHERTron <pritish.samal918@gmail.com>
2024-05-28 12:18:26 +0530

    feat: add controller to fetch pipeline yaml

--- server/api/FetchAllPipelines.js
+++ server/api/FetchAllPipelines.js
@@ -1,11 +1,37 @@
 const { default: axios } = require('axios')
 
+const FetchPipelineYaml = async (accountId, pipelineId, orgId, projectId, apiKey) => {
+    const url = `https://app.harness.io/pipeline/api/pipelines/${pipelineId}`;
+
+    const res = await axios({
+        method: 'get',
+        url: url,
+        params: {
+            accountIdentifier: accountId,
+            orgIdentifier: orgId,
+            projectIdentifier: projectId,
+            pageSize: 1000
+        },
+        headers: {
+            "x-api-key": apiKey
+        },
+        data: {
+            filterType: "PipelineSetup"
+        }
+    })
+
+
+    // return res.data
+}
+
 const FetchAllPipelines = async () => {
     const url = `${process.env.HARNESS_BASE_URL}/pipeline/api/pipelines/list`;
     const accountId = process.env.HARNESS_ACCOUNT_ID;
     const apiKey = process.env.HARNESS_API_KEY;
     const orgIdentifier = "default";
     const projectIdentifier = "CD_Demo";
+
+    const pipelinesWithYaml = [];
 
     const res = await axios({
         method: 'post',
@@ -26,7 +52,18 @@ const FetchAllPipelines = async () => {
     })
 
     if(res.status) {
-        return res.data;
+        const pipelines = res.data.data.content || [];
+
+        pipelines.forEach(async (pipeline, idx) => {
+            
+
+            if(idx === 0) {
+                const pipelineYaml = await FetchPipelineYaml(accountId, "Tomcat_Build", orgIdentifier, projectIdentifier, apiKey);
+                console.log(pipelineYaml);
+            } 
+        })
+
+        return pipelines;
     } else {
         return {};
     }
