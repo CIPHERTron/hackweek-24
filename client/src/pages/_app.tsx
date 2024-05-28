6fd5343d96ac5853293e35d7a6965e362e652222
CIPHERTron <pritish.samal918@gmail.com>
2024-05-28 15:04:39 +0530

    fix: style fixes

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "~/store/ThemeProvider";
+import Navigation from "~/components/shared/Navigation";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<>
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     enableSystem
     disableTransitionOnChange
   >
+    <Navigation />
   <Component {...pageProps} />
   </ThemeProvider>
   </>);
