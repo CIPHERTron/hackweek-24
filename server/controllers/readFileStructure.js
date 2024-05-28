5da139920d071ff003251f363f3692eb0ab9a791
CIPHERTron <pritish.samal918@gmail.com>
2024-05-28 19:25:39 +0530

    feat: add logic to read files & create orgs, projects & pipelines

---
+++ server/controllers/readFileStructure.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
+// Function to read file content
+const readFileContent = (filePath) => {
+    return fs.promises.readFile(filePath, 'utf-8');
+};
+
+// Function to create the structured object
+const createStructure = async (baseDir) => {
+    const structure = { orgs: {} };
+
+    const orgDirs = await fs.promises.readdir(baseDir);
+
+    for (const org of orgDirs) {
+        const orgPath = path.join(baseDir, org);
+        if (fs.statSync(orgPath).isDirectory()) {
+            structure.orgs[org] = {};
+            const projectDirs = await fs.promises.readdir(orgPath);
+
+            for (const project of projectDirs) {
+                const projectPath = path.join(orgPath, project);
+                if (fs.statSync(projectPath).isDirectory()) {
+                    structure.orgs[org][project] = {};
+                    const pipelineFiles = await fs.promises.readdir(projectPath);
+
+                    for (const pipeline of pipelineFiles) {
+                        const pipelinePath = path.join(projectPath, pipeline);
+                        if (path.extname(pipeline) === '.yaml') {
+                            const pipelineKey = path.basename(pipeline, '.yaml');
+                            const pipelineContent = await readFileContent(pipelinePath);
+                            structure.orgs[org][project][pipelineKey] = pipelineContent;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return structure;
+};
+
+module.exports = createStructure
