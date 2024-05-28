96395f7c7e05f16413f78366100de0e514d87402
CIPHERTron <pritish.samal918@gmail.com>
2024-05-28 17:01:37 +0530

    feat: initial iteration of parser logic

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
+
+const parsePipelineYaml = require('./parser/parser.js');
 
 const FetchAllProjects = require('./api/FetchAllProjects.js')
 const FetchAllPipelines = require('./api/FetchAllPipelines.js')
@@ -8,11 +12,23 @@ const FetchAllOrgs = require('./api/FetchAllOrgs.js')
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.text({ type: 'text/yaml' }));
 
 app.get('/', async (req, res) => {
     const response = await FetchAllPipelines();
     res.json({response});
 })
+
+app.get('/parse-pipeline', (req, res) => {
+    const filePath = path.join(__dirname, 'data/pipeline.yaml');
+    const parsedData = parsePipelineYaml(filePath);
+
+    if (parsedData) {
+        res.json(parsedData);
+    } else {
+        res.status(400).send('Error parsing the YAML file');
+    }
+});
 
 app.listen(4567, () => {
     console.log("Server running in port 4567")
