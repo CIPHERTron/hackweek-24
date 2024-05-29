36f2f2cd0b6e949c6573649e8067e8f2ef2f8f77
CIPHERTron <pritish.samal918@gmail.com>
2024-05-29 08:51:18 +0530

    feat: add rule to check for security scan step

---
+++ server/rules/securityScanStep.js
@@ -0,0 +1,23 @@
+const parsePipelineYaml = require('../parser/parser.js');
+
+const checkCiStageWithSecurityScanStep = (yamlString) => {
+    const pipelineDetails = parsePipelineYaml(yamlString);
+
+    if (!pipelineDetails || !pipelineDetails.stages) {
+        return false;
+    }
+
+    for (const stage of pipelineDetails.stages) {
+        if (stage.type === 'CI') {
+            for (const step of stage.steps) {
+                if (step.type === 'AquaTrivy') {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+};
+
+module.exports = checkCiStageWithSecurityScanStep
