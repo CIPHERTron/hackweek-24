40f2b1492a89a450c61d7a881b9a03eb971caff5
CIPHERTron <pritish.samal918@gmail.com>
2024-05-29 10:10:20 +0530

    feat: add rollback step rule

---
+++ server/rules/rollbackStep.js
@@ -0,0 +1,36 @@
+const parsePipelineYaml = require('../parser/parser.js');
+
+const checkRollbackStep = (yamlString) => {
+    const pipelineDetails = parsePipelineYaml(yamlString);
+
+    if (!pipelineDetails) {
+        return false;
+    }
+
+    const prodDeploymentStage = pipelineDetails.stages.find(stage => {
+        return stage.type === 'Deployment' &&
+               stage.spec.environment &&
+               stage.spec.environment.environmentRef === 'prod';
+    });
+
+    if (!prodDeploymentStage) {
+        return false;
+    }
+
+    const k8sRollingDeployStep = prodDeploymentStage.steps.find(step => step.type === 'K8sRollingDeploy');
+    if (!k8sRollingDeployStep) {
+        return false;
+    }
+
+    if (!prodDeploymentStage.rollbackSteps || prodDeploymentStage.rollbackSteps.length === 0) {
+        return false;
+    }
+
+    const rollbackStepWithTemplateRef = prodDeploymentStage.rollbackSteps.find(rollbackStep => {
+        return rollbackStep.stepGroup && rollbackStep.stepGroup.template && rollbackStep.stepGroup.template.templateRef !== null;
+    });
+
+    return !!rollbackStepWithTemplateRef;
+};
+
+module.exports = checkRollbackStep
