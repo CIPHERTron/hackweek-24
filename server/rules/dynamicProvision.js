135fd6a4bcb42c67e962e687e1de003548fc21a0
CIPHERTron <pritish.samal918@gmail.com>
2024-05-29 09:25:17 +0530

    feat: add check for dynamic provisioning

---
+++ server/rules/dynamicProvision.js
@@ -0,0 +1,27 @@
+const parsePipelineYaml = require('../parser/parser.js');
+
+const checkForDynamicProvisioning = (yamlString) => {
+    const pipelineDetails = parsePipelineYaml(yamlString);
+
+    if (!pipelineDetails || !pipelineDetails.stages) {
+        return false;
+    }
+
+    for (const stage of pipelineDetails.stages) {
+        if (stage.type === 'Deployment' && stage.spec && stage.spec.environment && stage.spec.environment.environmentRef === "QA" && stage.spec.environment.provisioner) {
+            const provisionerSteps = stage.spec.environment.provisioner.steps || [];
+
+            if (provisionerSteps.length > 0) {
+                for (const step of provisionerSteps) {
+                    if (step.type === 'TerraformPlan' || step.type === 'TerraformApply') {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
+    return false;
+};
+
+module.exports = checkForDynamicProvisioning
